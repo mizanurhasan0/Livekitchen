cdaea10ab02a509de3b7560dabc97bf2119b3df9
mizanurhasan <mizanur@coredevs.ltd>
2022-11-25 18:22:44 +0600

    pagination

--- kitchine -Dashboard/src/Pages/Catalog/ProductList.jsx
+++ kitchine -Dashboard/src/Pages/Catalog/ProductList.jsx
@@ -3,11 +3,16 @@ import { tableDb } from "../../Components/Shares/StaticData";
 import Tables from "../../Components/Shares/Tables";
 
 const ProductList = () => {
+  const [currentPosts, setCurrentPosts] = useState(false);
+
   return (
     <div className="p-5 bg-head">
       <div className="bg-txt p-2 overflow-scroll rounded-md">
         <h1 className="text-4xl pb-5 capitalize text-default">Products</h1>
-        <Tables tblData={tableDb} />
+        <Tables tblData={currentPosts} />
+        <div className="block text-center py-5 space-x-1 ">
+          <Pagination setCurrentPosts={setCurrentPosts} />
+        </div>
       </div>
     </div>
   );
